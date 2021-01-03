f68dc56d18681cd0972508a1c92b461f79d9fb8f
AsherLecover <asherlec5@gmail.com>
2021-01-03 19:52:35 +0200

    initail commit

--- src/app/components/patients/patients.component.ts
+++ src/app/components/patients/patients.component.ts
@@ -1,69 +1,52 @@
-import { Component, Inject, OnInit, ViewChild, ElementRef , ChangeDetectorRef} from '@angular/core';
+import {
+  Component,
+  Inject,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  ChangeDetectorRef,
+} from '@angular/core';
 import { from, Observable } from 'rxjs';
-import { PatientsService } from '../../servises/patients.service'
-import { MedicalMainDataModel } from '../../models/medical-main-data-model'
+import { PatientsService } from '../../servises/patients.service';
+import { MedicalMainDataModel } from '../../models/medical-main-data-model';
+import { PatientsModel } from '../../models/patients-model';
 import { FormBuilder, Validators } from '@angular/forms';
 import * as Plotly from 'plotly.js/dist/plotly.js';
 
-
-
 @Component({
-
   selector: 'patients',
   templateUrl: './patients.component.html',
   styleUrls: ['./patients.component.css'],
-
 })
 export class PatientsComponent implements OnInit {
-   firstPlotTrace = {
-    x: [1,2,3,4],
-    y: [1,2,3,4],
-    type: 'contour',
-    name: 'yaxis data'
-};
-
- secondPlotTrace = {
-    x: [4,15,26,37],
-    y: [14,25,36,57],
-    mode: 'lines+markers',
-    name: 'yaxis2 data'
-};
-
- plotData = [this.firstPlotTrace, this.secondPlotTrace];
-
- layout = {
-    title: 'Double Plot Title',
-    height: 400,
-    width: 400,
-    yaxis: {title: 'Simple Contour Plot Axis', range: [-20, 20]},
-    yaxis2: {title: 'Line and Scatter Plot Axis', range: [-20, 20]}
-};
-
+  layoutGraph = {
+    title: {
+      text:'',
+      font: {
+        family: 'Courier New, monospace',
+        size: 24
+      },
+      xref: 'paper',
+      x: 0.05,
+    },
+    hovermode: 'closest',
+    text:'Plot Title',
+    xaxis: { title: 'Date ' },
+    yaxis: { title: '' },
+  };
 
   param: string = '';
 
-  @ViewChild('chart') chart:ElementRef;
-  @ViewChild('plotDiv') plotDiv:ElementRef;
-
-
-  graphData = [{ x: [], y: [], type: 'line' }];
-
-  graphDataTest = [{ x: [1,2,3,4,5], y: [1,2,3,4,5], type: 'line' }];
-
-
-  public graphRpm = {
-    data: this.graphData,
-    layout: { 
-      hovermode:'closest',
-      title:'<br> click on a point to plot an annotation',
-      xaxis:{zeroline:false, title: 'Time'},
-      yaxis:{zeroline:false, title: `${this.param}`}
-   }
-  };
+  @ViewChild('chart') chart: ElementRef;
+  @ViewChild('chartSecond') chartSecond: ElementRef;
+  @ViewChild('myDiv') myDiv: ElementRef;
 
 
 
-  PatientList: Observable<any>
+  graphData = [{ x: [], y: [], name: '', type: 'line' }];
+  graphDataSecondGraph = [{ x: [], y: [], name: '', type: 'line' }];
+
+  PatientList: Observable<any>;
   madicalsParams: string[] = [
     'MovementFast',
     'MovementSlow',
@@ -81,81 +64,138 @@ export class PatientsComponent implements OnInit {
     'rpm_average',
     'short_rpm_average',
     'signal_quality',
-    'user_id'
   ];
 
   patientId: string = '';
   myForm: any;
-  dateTest: any;
   disableBtn: boolean = true;
+  elemenGraph
+  showGraphSelected: boolean = false;
+  
+  // elemenGraph = this.chart.nativeElement;
+
+
 
   constructor(
     private patientsService: PatientsService,
-    private fb: FormBuilder) {
-    this.PatientList = this.patientsService.getAllPatients()
-
-
+    private fb: FormBuilder
+  ) {
+    this.PatientList = this.patientsService.getAllPatients();
   }
 
   ngOnInit(): void {
-    Plotly.newPlot('plotDiv', this.plotData, this.layout);
-
-
     this.myForm = this.fb.group({
       dateOfPatien: ['', Validators.required]
     });
   }
 
   selectPatient(event: any) {
+    this.PatientList.subscribe( (data:PatientsModel[]) => {
+      console.log(data);
+      this.layoutGraph.title.text = data.find( atient => atient.id ==  event.target.value).username;
+
+      
+    })
     this.patientId = event.target.value;
-    if(this.patientId != '' && this.param != '' && this.myForm.valid){
+    console.log(event);
+    
+    if (this.patientId != '' && this.param != '' && this.myForm.valid) {
       this.disableBtn = false;
     }
   }
 
-  chackFormValid(e){
-    if(this.patientId != '' && this.param != '' && this.myForm.valid){
+  chackFormValid(e) {
+    if (this.patientId != '' && this.param != '' && this.myForm.valid) {
       this.disableBtn = false;
     }
   }
-
-
 
   selectParam(event) {
     this.param = event.target.value;
     console.log(event.target.value);
-    
-    if(this.patientId != '' && this.param != '' && this.myForm.valid){
+
+    if (this.patientId != '' && this.param != '' && this.myForm.valid) {
       this.disableBtn = false;
     }
   }
 
   sendReq() {
-        const elemenGraph = this.chart.nativeElement;
-        let arr = [this.graphData ,this.graphDataTest]
-          Plotly.newPlot(elemenGraph, this.graphDataTest);
+    this.showGraphSelected = true;
+    console.log(this.showGraphSelected);
+    
+    this.layoutGraph.yaxis.title = this.param;
+    this.elemenGraph = this.chart.nativeElement;
+    Plotly.newPlot(this.elemenGraph, this.graphData, this.layoutGraph);
 
-    this.patientsService.getUserMedicalDataByParam(this.patientId, this.param,this.myForm.value.dateOfPatien)
-    .subscribe( (data: MedicalMainDataModel[]) => {
-      data.forEach( e => {
-        this.graphData[0].x.push(e['TimeStamp'])
-        this.graphData[0].y.push(e[this.param.toString()])
-        
-      })
-      arr = [this.graphData ,this.graphDataTest]
-      Plotly.newPlot(elemenGraph, arr);
-      console.log(this.graphData);
-      
-      this.dateTest = this.myForm.value.dateOfPatien;
-    })
+    this.patientsService
+      .getUserMedicalDataByParam(
+        this.patientId,
+        this.param,
+        this.myForm.value.dateOfPatien
+      )
+      .subscribe((data: MedicalMainDataModel[]) => {
+        data.forEach((e) => {
+          this.graphData[0].x.push(e['TimeStamp']);
+          this.graphData[0].y.push(e[this.param.toString()]);
+          this.graphData[0].name = this.param;
+        });
+        Plotly.redraw(this.elemenGraph, this.graphData, this.layoutGraph);
 
+      });
   }
 
-  newParamOnThisGraph(){
+  sendReqOnNewGraph() {
+    this.layoutGraph.yaxis.title = this.param;
+    this.elemenGraph = this.chartSecond.nativeElement;
+    Plotly.newPlot(this.elemenGraph, this.graphDataSecondGraph, this.layoutGraph);
 
+    this.patientsService
+      .getUserMedicalDataByParam(
+        this.patientId,
+        this.param,
+        this.myForm.value.dateOfPatien
+      )
+      .subscribe((data: MedicalMainDataModel[]) => {
+        data.forEach((e) => {
+          this.graphDataSecondGraph[0].x.push(e['TimeStamp']);
+          this.graphDataSecondGraph[0].y.push(e[this.param.toString()]);
+          this.graphDataSecondGraph[0].name = this.param;
+        });
+        Plotly.redraw(this.elemenGraph, this.graphDataSecondGraph, this.layoutGraph);
+        console.log(this.graphDataSecondGraph);
+
+      });
+  }
+
+  sendReqOnExistsGraph(){
+    this.layoutGraph.yaxis.title = this.param;
+    let data = [this.graphData[0],this.graphDataSecondGraph[0]]
+    Plotly.newPlot(this.elemenGraph, data, this.layoutGraph);
+
+    this.patientsService
+      .getUserMedicalDataByParam(
+        this.patientId,
+        this.param,
+        this.myForm.value.dateOfPatien
+      )
+      .subscribe((data: MedicalMainDataModel[]) => {
+        data.forEach((e) => {
+          this.graphDataSecondGraph[0].x.push(e['TimeStamp']);
+          this.graphDataSecondGraph[0].y.push(e[this.param.toString()]);
+          this.graphDataSecondGraph[0].name = this.param;
+        });
+        Plotly.redraw(this.elemenGraph,data, this.layoutGraph);
+        console.log(this.graphDataSecondGraph);
+      });
+  }
+
+  clearChars(){
+    Plotly.deleteTraces(this.elemenGraph, 0);
   }
 
  
+
  
+
 
 }
---
+++ src/app/models/patients-model.ts
@@ -0,0 +1,6 @@
+export class PatientsModel{
+    id:number;
+    username:string;
+    age:string;
+    address:string;
+}
